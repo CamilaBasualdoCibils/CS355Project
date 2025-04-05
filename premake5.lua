a342391fd960e9126875a01836665f818568b950
CamilaBasualdoCibils <camila.basualdocibils@gmail.com>
2025-04-05 16:18:06 -0700

    initial

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+-- premake5.lua
+workspace "Digipen"
+   configurations { "Debug", "Release" }
+
+project "Mergesort"
+
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++20"
+   targetdir "bin/%{cfg.buildcfg}"
+   flags{"MultiProcessorCompile"}
+
+   files {"src/**.c","src/**.cpp"}
+   pchheader "pch.hpp"
+   includedirs {"src","lib"}
+   --links { }
+
+
+   filter "configurations:Debug"
+      defines { "_DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "_NDEBUG","_RELEASE" }
+      optimize "On"
+
+   --filter "action:gmake" IMPORTANT FOR COMPILING IN LINUX FOR WINDOWS
+      --gccprefix "x86_64-w64-mingw32-" --compilation in linux for windows
+
---
+++ src/main.cpp
@@ -0,0 +1,4 @@
+#include <pch.hpp>
+int main() {
+
+}

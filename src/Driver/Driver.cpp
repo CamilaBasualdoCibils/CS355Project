14a55a2c11bcc2a155b6c33f4188acea5c41809e
CamilaBasualdoCibils <camila.basualdocibils@gmail.com>
2025-04-12 17:26:35 -0700

    Driver improvement

--- src/Driver/Driver.cpp
+++ src/Driver/Driver.cpp
@@ -1,13 +1,32 @@
 #include "Driver.hpp"
 #include <mergesort.h>
-std::chrono::milliseconds Driver::IotaIntTestTimed(uint32_t num_count, bool shuffled)
+std::chrono::milliseconds Driver::IotaIntTestTimed(uint32_t num_count, bool shuffled,uint32_t thread_count)
 {
     std::vector<int> vec(num_count);
     std::iota(vec.begin(), vec.end(), 0);
     if (shuffled)
         std::shuffle(vec.begin(), vec.end(), std::mt19937{std::random_device{}()});
 
-    return IntTestTimed(vec.begin(), vec.end());
+    return TestTimed(vec.begin(), vec.end());
+}
+
+std::chrono::milliseconds Driver::StringTestTimed(uint32_t num_strings, uint32_t stringLength, uint32_t max_thread_count)
+{
+    static const std::string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    static std::mt19937 rng(static_cast<unsigned>(std::time(nullptr))); // Mersenne Twister RNG
+    static std::uniform_int_distribution<> dist(0, charset.size() - 1);
+    std::vector<std::string> strings;
+    strings.resize(num_strings);
+    for (int s = 0; s < num_strings; s++)
+    {
+        std::string &result = strings[s];
+        result.reserve(stringLength);
+        for (size_t i = 0; i < stringLength; ++i)
+        {
+            result += charset[dist(rng)];
+        }
+    }
+    return TestTimed(strings.begin(), strings.end());
 }
 
 Driver::Driver()
@@ -30,6 +49,42 @@ Driver::Driver()
         { IotaIntTestTimed(60, true); },
         [&]()
         { IotaIntTestTimed(90, true); },
+
+        [&]()
+        { StringTestTimed(3, 3, 1); },
+        [&]()
+        { StringTestTimed(7, 5, 1); },
+        [&]()
+        { StringTestTimed(548, 85, 1); },
+        [&]()
+        { StringTestTimed(3, 1687, 1); },
+        [&]()
+        { StringTestTimed(1000, 1000, 1); },
+
+        [&]()
+        { StringTestTimed(3, 3, 3); },
+        [&]()
+        { StringTestTimed(7, 5, 3); },
+        [&]()
+        { StringTestTimed(548, 85, 3); },
+
+        [&]()
+        { StringTestTimed(3, 3, SYSTEM_THREAD_COUNT); },
+        [&]()
+        { StringTestTimed(7, 5, SYSTEM_THREAD_COUNT); },
+        [&]()
+        { StringTestTimed(548, 85, SYSTEM_THREAD_COUNT); },
+        [&]()
+        { StringTestTimed(1000, 1000, SYSTEM_THREAD_COUNT); },
+        [&]()
+        { StringTestTimed(10000, 10000, SYSTEM_THREAD_COUNT); },
+        [&]()
+        { StringTestTimed(3, 3, 64); },
+        [&]()
+        { StringTestTimed(7, 5, 64); },
+        [&]()
+        { StringTestTimed(548, 85, 64); },
+
     };
 }
 

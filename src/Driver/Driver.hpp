15d05bb6fdc00515f824c7c50ad7b1fe02271d19
CamilaBasualdoCibils <camila.basualdocibils@gmail.com>
2025-04-12 17:26:35 -0700

    Driver improvement

--- src/Driver/Driver.hpp
+++ src/Driver/Driver.hpp
@@ -1,24 +1,31 @@
 #include <pch.hpp>
 #include <mergesort.h>
-class Driver {
-    std::chrono::milliseconds IotaIntTestTimed(uint32_t num_count, bool shuffled);
-    template <typename _RAIter>
-    std::chrono::milliseconds IntTestTimed(_RAIter begin, _RAIter end);
+#include <thread>
+class Driver
+{
 
-    template <typename _RAIter>
-    std::chrono::milliseconds OurMergeSortTimed(const MergeSortSettings &sett, _RAIter begin, _RAIter end);
+ 
 
 public:
+std::chrono::milliseconds IotaIntTestTimed(uint32_t num_count, bool shuffled,uint32_t thread_count = 1);
+template <typename _RAIter>
+std::chrono::milliseconds TestTimed(_RAIter begin, _RAIter end,uint32_t thread_count = 1);
+
+template <typename _RAIter>
+std::chrono::milliseconds OurMergeSortTimed(const MergeSortSettings &sett, _RAIter begin, _RAIter end);
+
+std::chrono::milliseconds StringTestTimed(uint32_t num_strings, uint32_t stringLength, uint32_t max_thread_count = 1);
     Driver();
     void RunAllTests();
     void RunTest(std::uint32_t test_index);
 
 private:
+    const static inline uint32_t SYSTEM_THREAD_COUNT = std::thread::hardware_concurrency();
     std::vector<std::function<void(void)>> tests;
 };
 
 template <typename _RAIter>
-inline std::chrono::milliseconds Driver::IntTestTimed(_RAIter begin, _RAIter end)
+inline std::chrono::milliseconds Driver::TestTimed(_RAIter begin, _RAIter end,uint32_t thread_count)
 {
     MergeSortSettings settings;
     std::stringstream orig, after;
@@ -37,10 +44,10 @@ inline std::chrono::milliseconds Driver::IntTestTimed(_RAIter begin, _RAIter end
             after << *it << " ";
         after << "\n";
         std::cout << orig.str() << after.str();
-        
-        DEBUG_BREAK(); //FAILURE BREAKPOINT
+
+        DEBUG_BREAK(); // FAILURE BREAKPOINT
     }
-    
+
     assert(sorted && "WRONG RUN");
     return duration;
 }
@@ -48,10 +55,10 @@ inline std::chrono::milliseconds Driver::IntTestTimed(_RAIter begin, _RAIter end
 template <typename _RAIter>
 inline std::chrono::milliseconds Driver::OurMergeSortTimed(const MergeSortSettings &sett, _RAIter begin, _RAIter end)
 {
-    auto start_t= std::chrono::high_resolution_clock::now();
-    //std::stable_sort(begin,end);
-    mergesortsinglethread(sett,begin,end);
-    auto end_t= std::chrono::high_resolution_clock::now();
+    auto start_t = std::chrono::high_resolution_clock::now();
+    // std::stable_sort(begin,end);
+    mergesortsinglethread(sett, begin, end);
+    auto end_t = std::chrono::high_resolution_clock::now();
 
     return std::chrono::duration_cast<std::chrono::milliseconds>(end_t - start_t);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,53 @@
 #include <pch.hpp>
 #include "Driver/Driver.hpp"
+#include <fstream>
 int main()
 {
     Driver driver;
     driver.RunAllTests();
+
+
+std::stringstream ss;
+
+
+const uint64_t max_thread_test = 32;
+const uint64_t Test_count_each = 1000;
+const uint64_t N = 100000;
+for (int thr_cnt = 1; thr_cnt <= max_thread_test;thr_cnt++) {
+
+    const uint64_t progress_bar_char_count = max_thread_test;
+    const float progress =  (float(thr_cnt)/max_thread_test);
+    const int char_filled = progress*progress_bar_char_count;
+    std::cout << "\r[";
+    for (int i = 0; i < char_filled;i++) {
+        std::cout << 'X';
+    }
+    for (int i = 0; i < progress_bar_char_count-char_filled;i++) {
+        std::cout << '-';
+    }
+    std::cout << "] " <<(int)(progress*100)<<"%    " << std::flush;
+
+    for (int test_count = 0; test_count< Test_count_each;test_count++ ) {
+        auto time = driver.IotaIntTestTimed(N,true,thr_cnt);
+        ss << thr_cnt << "," 
+        << std::fixed << std::setprecision(6) << std::chrono::duration<double>(time).count()
+        <<std::endl;
+    }
+}
+std::cout << "\n";
+
+
+    
+std::ofstream csvFile("mergesort.csv");
+if (!csvFile.is_open()) {
+    std::cerr << "Error: Could not open file!" << std::endl;
+    return 1;
+}
+
+    // Append the data to the file
+csvFile << ss.str();
+
+
+// close it somewhere
+csvFile.close();
 }
